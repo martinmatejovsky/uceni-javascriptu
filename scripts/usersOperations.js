0c576ce8af376cf32f4d192f82a3b6ff03288f47
Martin Prochazka <martin.prochazka84@seznam.cz>
2022-12-31 11:26:48 +0100

    First 6 pints from 9 operations with the array

--- scripts/usersOperations.js
+++ scripts/usersOperations.js
@@ -2,20 +2,30 @@ const applicationPage = document.getElementById('application')
 let newUsers = ['Jan Honza', 'Karel Gott', 'Josef Pepa']
 const editedUser =  ['2', 'Mgr. Alexandr Saša']
 
+let tempUsers = users.replace('[','').replace(']','').replace(/[\n"]+/gm, '')
+console.log(tempUsers)
+let helpUsers = tempUsers.split(',')
+for (let i = 0; i < helpUsers.length; i++){
+    helpUsers[i] = helpUsers[i].trimStart()     //odstrani mezery na zacatku stringu
+}
+let checkNumberOfUsers = helpUsers.length
+
 // 1 - proměnná "users" je databáze uživatelů
 applicationPage.innerHTML = '1 - ' + users + '<hr>'
 
 // 2 - zjisti počet uživatelů v databázi
 let numberOfUsers = '3'
-applicationPage.insertAdjacentHTML('beforeend', '2 - ' + numberOfUsers + '<hr>')
+applicationPage.insertAdjacentHTML('beforeend', '2 - ' + numberOfUsers + '; My check is: ' + checkNumberOfUsers +'<hr>')
 
 // 3 - vypiš posledního uživatele z databáze
 let lastUser = 'Alexandr Saša'
-applicationPage.insertAdjacentHTML('beforeend', '3 - ' + lastUser + '<hr>')
+let checkLastUser = helpUsers[checkNumberOfUsers - 1]
+applicationPage.insertAdjacentHTML('beforeend', '3 - ' + lastUser + '; My check is: ' + checkLastUser + '<hr>')
 
 // 4 - vypiš index (pozici) uživatele "Jan Honza"
 let searchedUserIndex = '0'
-applicationPage.insertAdjacentHTML('beforeend', '4 - ' + searchedUserIndex + '<hr>')
+let positionMatch = helpUsers.indexOf("Jan Honza")   // .indexOf ()vraci pozici , pokud da -1, tak nenaslo shodu
+applicationPage.insertAdjacentHTML('beforeend', '4 - ' + searchedUserIndex + '; My check is: ' + positionMatch + '<hr>')
 
 // 5 - přidej do pole users uživatele 'Prokop Buben' na poslední pozici
 users = [
@@ -24,13 +34,20 @@ users = [
     "Alexandr Saša",
     'Prokop Buben'
 ]
-applicationPage.insertAdjacentHTML('beforeend', '5 - ' + users + '<hr>')
+helpUsers.push('Prokop Buben')
+applicationPage.insertAdjacentHTML('beforeend', '5 - ' + users + '; My check is: ' + helpUsers + '<hr>')
 
 // 6 - máme pole editedUser. Jeho první položka říká index uživatele a druhá je jeho nové jméno. Vyměň uživatele v poli users na daném indexu za nové jméno
-applicationPage.insertAdjacentHTML('beforeend', '6 - ' + users + '<hr>')
+helpUsers[editedUser[0]] = editedUser[1]
+applicationPage.insertAdjacentHTML('beforeend', '6 - ' + users + '; My check is: ' + helpUsers + '<hr>')
 
 // 7 - napiš true/false za každou osobu v poli newUsers podle toho, jestli už se nacházejí v poli users
 let userFound = 'true false true'
+// let positionMatch = helpUsers.indexOf("Jan Honza")
+// let newUsers = ['Jan Honza', 'Karel Gott', 'Josef Pepa']
+for (let i = 0; i < newUsers.length; i++){
+    
+}
 applicationPage.insertAdjacentHTML('beforeend', '7 - ' + userFound + '<hr>')
 
 // 8 - opět z pole newUsers projdi jednotlivé osoby a pokud ještě nejsou v poli users, tak je tam přidej na poslední pozici

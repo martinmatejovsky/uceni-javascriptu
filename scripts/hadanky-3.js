2df26d061c448e7acebeb7e4fa1ef4ded2dc735b
godnat <26278424+godnat@users.noreply.github.com>
2024-11-04 21:14:45 +0100

    nada test

--- scripts/hadanky-3.js
+++ scripts/hadanky-3.js
@@ -68,8 +68,14 @@ works for any size, outputting a grid of the given width and height.
 Napiš funkci, která přijme pole čísel a vrátí největší číslo.
 */
 let randomNumbers = [2, 2, 8, 123, 1, -1]
+let nejvetsi = randomNumbers[0];
 
-
+for (i=0; i < randomNumbers.length; i++) {
+	if (randomNumbers[i] > nejvetsi) {
+		nejvetsi = randomNumbers[i];
+  }
+}
+console.log(nejvetsi);
 
 /*
 ====================================================================================
@@ -78,10 +84,15 @@ let randomNumbers = [2, 2, 8, 123, 1, -1]
 Napiš funkci, která vytvoří nové pole (treba jmenem badGrades), ve kterém budou jen ta čísla, co jsou větší než 3.
 Jako vstup si vezmeme proměnnou znamkyVeSkole
 */
-let gradesAtSchool = [1, 5, 5, 4, 5, 2, 1, 1, 1, 2]
+let gradesAtSchool = [1, 5, 5, 4, 5, 2, 1, 1, 1, 2];
+let badGrades = [];
+for (i=0; i < gradesAtSchool.length; i++) {
+    if (gradesAtSchool[i] > 3) {
+        badGrades.push(gradesAtSchool[i]);
+    }
+}
 
-
-
+console.log(badGrades);
 /*
 ====================================================================================
 # Hrdinové socialistické práce 1

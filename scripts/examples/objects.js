19f50eaeae2853d9f0dc65570e27c8164892195b
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-11-18 21:09:17 +0100

    created new example on Objects data type

---
+++ scripts/examples/objects.js
@@ -0,0 +1,55 @@
+"neco"
+let myNumbner = 321
+let myArray = ["neco", "neco jineho"] // length
+
+
+
+// object
+
+// {
+//     key: "data nejaka libovolna",
+// }
+
+let startCar = function () {
+    console.log("brm brmmm")
+}
+
+let myCar = {
+    productionYear: 1999, // vlastnost, property
+    passenger: ['Osoba A', 'Osoba B'],
+    manufacturer: "Skoda auto",
+    model: "Fabia",
+    startCar: function () { // metoda, method
+        console.log("brm brmmm")
+    },
+}
+
+// console.log('Method from our object')
+// console.log(myCar.startCar)
+
+// console.log('Method from value nehanced bz javascrippt itself')
+// console.log(myArray.push)
+
+console.log("ahoj")
+
+let requetedKey;
+requetedKey = 'model'
+
+// console.log(myCar.model)
+// console.log(myCar[requetedKey])
+
+// myCar.passenger.push("Osoba C")
+
+// console.log(myCar.passenger)
+
+// let model = "Fabia"
+// model = "Octavia"
+
+// myCar.model = "Octavia"
+
+// console.log(typeof myCar.model)
+
+// myCar.model = ["Octavia", "Fabia"]
+
+// console.log(myCar.model)
+

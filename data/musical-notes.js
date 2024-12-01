6a5f22f767da39cf49afa426416133d0a5db4472
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-12-01 15:28:43 +0100

    Created dataset of musical notes

---
+++ data/musical-notes.js
@@ -0,0 +1,52 @@
+const availableNotes = [
+  {
+    "id": "c",
+    "name": "C"
+  },
+  {
+    "id": "c#",
+    "name": "C#"
+  },
+  {
+    "id": "d",
+    "name": "D"
+  },
+  {
+    "id": "d#",
+    "name": "D#"
+  },
+  {
+    "id": "e",
+    "name": "E"
+  },
+  {
+    "id": "f",
+    "name": "F"
+  },
+  {
+    "id": "f#",
+    "name": "F#"
+  },
+  {
+    "id": "g",
+    "name": "G"
+  },
+  {
+    "id": "g#",
+    "name": "G#"
+  },
+  {
+    "id": "a",
+    "name": "A"
+  },
+  {
+    "id": "a#",
+    "name": "A#"
+  },
+  {
+    "id": "b",
+    "name": "B"
+  }
+]
+
+export default availableNotes

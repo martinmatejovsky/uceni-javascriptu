7b2757ba68d3aee63b375234819c71619bfede6b
Foldas <Foldas@seznam.cz>
2024-12-06 21:54:57 +0100

    postupné uložení aplikacce

--- scripts/ChordsApp/guitarChordsApp.js
+++ scripts/ChordsApp/guitarChordsApp.js
@@ -1,1 +1,12 @@
 //javascript, který bude provádět akci, bude tahat data z jiných JS, bude navázaný na HTML stránku, bude porvádět random kombinaci prvků a vypíše je na stránku
+
+import avaliableChordsBase from "../data/chordsGuitar.js";
+
+const buttonShowNext = document.getElementById("guitar-start-exercise");
+const application = document.getElementById("app");
+
+const buttonScream = () => {
+  application.innerHTML = "C";
+};
+
+buttonShowNext.addEventListener("click", buttonScream);

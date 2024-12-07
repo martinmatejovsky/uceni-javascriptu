53eb0809f49bd6e0f526ed3d06373db70da9bc4d
Foldas <Foldas@seznam.cz>
2024-12-07 01:05:17 +0100

    GChAp base concept works - random chords are written after click

--- data/chordsGuitar.js
+++ data/chordsGuitar.js
@@ -1,33 +1,32 @@
 const chordsGuitarBasic = [
-    {
-        "id": "c",
-        "name": "C"
-    },
-    {
-        "id": "d",
-        "name": "D"
-    },
-    {
-        "id": "e",
-        "name": "E"
-    },
-    {
-        "id": "f",
-        "name": "F"
-    },
-    {
-        "id": "g",
-        "name": "G"
-    },
-    {
-        "id": "a",
-        "name": "A"
-    },
-    {
-        "id": "h",
-        "name": "H"
-    }
+  {
+    id: "c",
+    name: "C",
+  },
+  {
+    id: "d",
+    name: "D",
+  },
+  {
+    id: "e",
+    name: "E",
+  },
+  {
+    id: "f",
+    name: "F",
+  },
+  {
+    id: "g",
+    name: "G",
+  },
+  {
+    id: "a",
+    name: "A",
+  },
+  {
+    id: "h",
+    name: "H",
+  },
+];
 
-]
-
-export default chordsGuitarBasic //co toto dělá?
+export default chordsGuitarBasic;
--- scripts/ChordsApp/guitarChordsApp.js
+++ scripts/ChordsApp/guitarChordsApp.js
@@ -1,12 +1,18 @@
 //javascript, který bude provádět akci, bude tahat data z jiných JS, bude navázaný na HTML stránku, bude porvádět random kombinaci prvků a vypíše je na stránku
 
-import avaliableChordsBase from "../data/chordsGuitar.js";
+import chordsGuitarBasic from "../../data/chordsGuitar.js";
 
-const buttonShowNext = document.getElementById("guitar-start-exercise");
+const buttonStart = document.getElementById("guitar-start-excercise");
 const application = document.getElementById("app");
 
-const buttonScream = () => {
-  application.innerHTML = "C";
+// funkce na volání random hodnoty z pole chordsGuitarBasic, které je definováno v chordsGuitar.js
+const callRandomChord = function () {
+  const minValue = 0;
+  const maxValue = chordsGuitarBasic.length - 1;
+  //const chordValue = chordsGuitarBasic[Math.floor(Math.random() * (maxValue - minValue + 1))];
+// application.innerHTML = chordValue.name;
+  application.innerHTML = chordsGuitarBasic[Math.floor(Math.random() * (maxValue - minValue + 1))].name;
 };
 
-buttonShowNext.addEventListener("click", buttonScream);
+//přiřazení akce po kliknutí na tlačítko
+buttonStart.addEventListener("click", callRandomChord);

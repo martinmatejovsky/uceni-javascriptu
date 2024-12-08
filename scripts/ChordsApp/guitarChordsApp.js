6da2069598e53d87fc4cd712024bddb31c30f53d
Foldas <Foldas@seznam.cz>
2024-12-08 17:05:59 +0100

    Double button Start-Stop - done

--- scripts/ChordsApp/guitarChordsApp.js
+++ scripts/ChordsApp/guitarChordsApp.js
@@ -3,27 +3,61 @@
 import chordsGuitarBasic from "../../data/chordsGuitar.js";
 import chordsGuitarAdds from "../../data/chordsGuitarAdds.js";
 
+//nastavení tlačíka start/stop
 const buttonStart = document.getElementById("guitar-start-excercise");
+//nastavení prostoru (podle matesa) kde se bude vypisovat výsledek funkce ShowRandomResult, tedy náhodný akord
 const application = document.getElementById("app");
 
 // funkce na volání random hodnoty z pole chordsGuitarBasic, které je definováno v chordsGuitar.js
 const callRandomChord = function () {
   const minValue = 0;
   const maxValue = chordsGuitarBasic.length - 1;
-  //const chordValue = chordsGuitarBasic[Math.floor(Math.random() * (maxValue - minValue + 1))];
-// application.innerHTML = chordValue.name;
   return chordsGuitarBasic[Math.floor(Math.random() * (maxValue - minValue + 1))].name;
 };
 
+//funkce na volání random hodnotot z pole chordsGuitarAdd
 const callRandomChordAdd = function() {
   const minValue = 0;
   const maxValue = chordsGuitarAdds.length - 1;
   return chordsGuitarAdds[Math.floor(Math.random() * (maxValue - minValue + 1))].name;
 }
 
+//sloučení obou random hodnot dohromady, tedy námi požadovaný výsledek
 const showRandomResult = function(){
   application.innerHTML = callRandomChord() + " " + callRandomChordAdd();
 };
 
+// opakování random funkce v časovém intervalu
+let intervalID;
+const repeatFunction = function(){
+  showRandomResult();
+  if (!intervalID){
+    intervalID = setInterval(showRandomResult, 5000);
+  }
+}
+//ukončení opakování random funkce
+const stopRepeatFunction = function(){
+  if (intervalID) {
+    clearInterval(intervalID);
+  }
+}
+
+//změna funkce tlačítka guitar-sart-excercise - snaha o změnu po kliknutí - Start-Stop
+let firstClick = function(){
+  repeatFunction();
+  buttonStart.removeEventListener("click", firstClick);
+  buttonStart.textContent = "STOP the ROCK!!!";
+  buttonStart.addEventListener("click", secondClick);
+  }
+  
+let secondClick = function(){
+  stopRepeatFunction();
+  buttonStart.textContent = "Lets rock";
+  buttonStart.removeEventListener("click", secondClick);
+  buttonStart.addEventListener("click", firstClick);
+  }
+
 //přiřazení akce po kliknutí na tlačítko
-buttonStart.addEventListener("click", showRandomResult);
+buttonStart.addEventListener("click", firstClick);
+
+

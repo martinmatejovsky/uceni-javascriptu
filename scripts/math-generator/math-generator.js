d8528dea247df77c43576f5b3c002fcbccc8e1aa
KorinMP <martin.prochazka84@seznam.cz>
2025-01-13 19:56:40 +0100

    Add nav to html and separate function to create example

--- scripts/math-generator/math-generator.js
+++ scripts/math-generator/math-generator.js
@@ -9,6 +9,34 @@ function randomNumber(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
+function addiction() {
+    let firstAddictionNumber = randomNumber(10);
+    let secondAddictionNumber = randomNumber(10);
+    return (firstAddictionNumber + " + " + secondAddictionNumber);
+}
+
+function substraction() {
+    let firstSubstractionNumber = randomNumber(100);
+    let secondSubstractionNumber = randomNumber(100);
+    if (firstSubstractionNumber < secondSubstractionNumber) {
+        return (secondSubstractionNumber + " - " + firstSubstractionNumber);
+    }
+    return (firstSubstractionNumber + " - " + secondSubstractionNumber);
+}
+
+function multiple() {
+    let firstMultipleNumber = randomNumber(10);
+    let secondMultipleNumber = randomNumber(10);
+    return (firstMultipleNumber + " * " + secondMultipleNumber);
+}
+
+function division() {
+    let firstDivisionNumber = randomNumber(10);
+    let secondDivisionNumber = randomNumber(10);
+    let thirdDivisionNumber = firstDivisionNumber * secondDivisionNumber;
+    return (thirdDivisionNumber + " / " + firstDivisionNumber);
+}
+/*
 function randomMathExample() {
 
     let mathSign = randomNumber(3);
@@ -30,3 +58,4 @@ function randomMathExample() {
         }
     }
 }
+*/

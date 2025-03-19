025c3c4366e68419903dad0e5436e737a5116384
KorinMP <martin.prochazka84@seznam.cz>
2025-03-19 18:18:55 +0100

    function version of addiction. issue with decimal point

--- scripts/math-generator/math-generator.js
+++ scripts/math-generator/math-generator.js
@@ -1,37 +1,63 @@
 const buttonStart = document.getElementById("Start");
-const application = document.getElementById("app")
-//document.getElementById("example-type").addEventListener("change", function (event) {
-//    const selectedOption = this.options[this.selectedIndex];
-//    console.log(selectedOption);
-//    const rangeData = selectedOption.getAttribute("data-range");
-//    console.log(rangeData);
-//    const parsedData = JSON.parse(rangeData)
-//    console.log("Check: ", parsedData)
-//    event.preventDefault()
-//})
+const application = document.getElementById("app");
 
 let dataSet;
-let dataRange
-let dataParsed 
+let dataRange;
+let dataParsed;
+let firstNumberToCountRange;
+let secondNumberToCountRange;
+let multipleFirstNum;
+let multipleSecondNum;
+let operand;
 
-buttonStart.addEventListener("click", function(event) {
-    event.preventDefault();                                                                     //prevent to reset page to default state. All JS action will just flash on the page
-    let selectedTag = document.getElementsByClassName("selectedOptions")                        //load all class="selectedOptions" to later check
+//let availableOperands = ["+","-","*","/"]
+
+function randomIntFromInterval(min, max) {                                                      // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function fillData() {
+    let selectedTag = document.getElementsByClassName("selectedOptions");                       //load all class="selectedOptions" to later check
     
     for (let i = 0; i < selectedTag.length; i++){
         if (selectedTag[i].value != "0") {                                                      //check if selected value is != 0. it means that some option was selected
             const selectedOption = selectedTag[i].options[selectedTag[i].selectedIndex];        //found option with the same value like was selected
             const dataRange = selectedOption.getAttribute('data-range');                        //load data from data-range as string
             dataParsed = JSON.parse(dataRange);                                                 //change string to objet
-            console.log(dataParsed)
+            console.log(dataParsed);
         }
     }
+
+    firstNumberToCountRange = dataParsed.firstNumber;
+    secondNumberToCountRange = dataParsed.secondNumber;
+    multipleFirstNum = dataParsed.multipleFirstNum;
+    multipleSecondNum = dataParsed.multipleSecondNum;
+    operand = dataParsed.operand;
+}
+
+function writeExercices() {
+//    let selectedOperand = availableOperands[operand]
+
+    let firstNumber = randomIntFromInterval(firstNumberToCountRange[0], firstNumberToCountRange[1])
+    let secondNumber = randomIntFromInterval(secondNumberToCountRange[0], secondNumberToCountRange[1])
+
+    if (operand == 0){                                                                           //addiction
+        return ((firstNumber * multipleFirstNum) + " + " + (secondNumber * multipleSecondNum))
+    } else if (operand == 1){                                                                    //Subtraction
+        return ((firstNumber * multipleFirstNum) + " - " + (secondNumber * multipleSecondNum))
+    } else if (operand == 2){                                                                    //Multiplication
+        return ((firstNumber * multipleFirstNum) + " * " + (secondNumber * multipleSecondNum))
+    } else if (operand == 3){                                                                    //Division
+        return ((firstNumber * multipleFirstNum) + " / " + (secondNumber * multipleSecondNum))
+    }
+}
+
+buttonStart.addEventListener("click", function(event) {
+    event.preventDefault();                                                                      //prevent to reset page to default state. All JS action will just flash on the page
+    
+    fillData(event);
+    application.innerHTML = writeExercices();
 });
-
-
-
-
-
 
 
 /*

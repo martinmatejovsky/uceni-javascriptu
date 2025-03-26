a20defb9c6e4d80f3e644908e355f340ce486408
Foldas <Foldas@seznam.cz>
2025-03-26 21:31:55 +0100

    drobne bezvyznamne upravy css, a odkaz na script v HTML

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,58 +1,58 @@
-console.log('File variables.js loaded');
+console.log("File variables.js loaded");
 
-let a = "Rekl jsem 'Ahoj'"
+let a = "Rekl jsem 'Ahoj'";
 
-let nakupniSeznam = ["cibule", [], "brambory"]
+let nakupniSeznam = ["cibule", [], "brambory"];
 
-console.log(typeof nakupniSeznam) // "Object"
+console.log(typeof nakupniSeznam); // "Object"
 
-console.log(nakupniSeznam[0])
+console.log(nakupniSeznam[0]);
 
-nakupniSeznam.length
+nakupniSeznam.length;
 
-a.length
+a.length;
 
 function sumUpNumbers(/* parametry funkce */) {
-    // telo funkce
+  // telo funkce
 }
 
 let mySumFunction = (a, b) => {
-    let result = a * b
-    console.log(result)
-}
+  let result = a * b;
+  console.log(result);
+};
 
-mySumFunction(8, 5)
-
+mySumFunction(8, 5);
 
 // podminky
 if (nakupniSeznam.length < 10) {
-    // kod, ktery se ma provest, kdyz podminka plati
+  // kod, ktery se ma provest, kdyz podminka plati
 } else if (nakupniSeznam.length < 50) {
-    // kod, ktery se ma provest, kdyz podminka plati
+  // kod, ktery se ma provest, kdyz podminka plati
 } else {
-    // kod, ktery se ma provest, kdyz podminka plati
+  // kod, ktery se ma provest, kdyz podminka plati
 }
 
-
 for (let i = 0; i >= 5; i + 1) {
-    // neco proved
+  // neco proved
 
-    if (nakupniSeznam.length < i) {
-        break
-    }
+  if (nakupniSeznam.length < i) {
+    break;
+  }
 
-    console.log(nakupniSeznam[i])
+  console.log(nakupniSeznam[i]);
 }
 
 function transferBladder(earthWorm) {
-    // earthWorm = 15
-    let bladders = 0;
-    let remainingWorms = 0;
+  // earthWorm = 15
+  let bladders = 0;
+  let remainingWorms = 0;
 
-    bladders = Math.floor(earthWorm / 7)
-    remainingWorms = earthWorm % 7 // => 7 + 7 + 1
+  bladders = Math.floor(earthWorm / 7);
+  remainingWorms = earthWorm % 7; // => 7 + 7 + 1
 
-    console.log('vysledek je ' + bladders + ' mechyru ' + remainingWorms + ' zizal.')
+  console.log(
+    "vysledek je " + bladders + " mechyru " + remainingWorms + " zizal."
+  );
 }
 
-transferBladder(15)
+transferBladder(15);
